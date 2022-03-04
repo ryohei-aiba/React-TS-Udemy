db18bfa380d9d711511843cd6a15a6a391b79120
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-03-05 02:52:55 +0900

    return type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Practice1 } from "./practices/practice1";
+import { Practice2 } from "./practices/practice2";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
     </div>
   );
 }
