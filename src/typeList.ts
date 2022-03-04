f1d879f5dfea22a6ea9411a527f58fd437ab1c0f
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-03-05 02:29:03 +0900

    Learning type

---
+++ src/typeList.ts
@@ -0,0 +1,25 @@
+/*eslint-disable @typescript-eslint/no-unused-vars */
+
+let bool: boolean = true;
+
+let num: number = 0;
+
+let str: string = "morikeTsu";
+
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+let tuple: [string, number] = ["gorillaz", 1];
+
+let any1: any = false;
+
+const func1 = (): void => {
+  const name = "Gorillaz";
+};
+
+let null1: null = null;
+
+let undefined1: undefined = undefined;
+
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "gorillaz" };
